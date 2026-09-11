367cb234dd25b32489ac753cfd5d3061e6f502e6
Vini <marcos646362@gmail.com>
2026-09-11 20:10:03 -0300

    add/added site map

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import Sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
   base: "https://vinimesquitamoraes.github.io/web_sites_deploy_test/",
@@ -10,6 +11,15 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    Sitemap({
+      hostname: 'https://vinimesquitamoraes.github.io/web_sites_deploy_test/',
+      dynamicRoutes: [
+        '/about',
+        '/FAQ',
+        '/download',
+        '/credits'
+      ]
+    })
   ],
   resolve: {
     alias: {
